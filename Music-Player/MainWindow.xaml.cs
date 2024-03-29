4a60be74acc4566d0beb65a6fbc8bc58386a64e3
Angel Quintero <angelqv2014@gmail.com>
2024-03-29 02:38:45 -0400

    Songs menu UI finished

--- Music-Player/MainWindow.xaml.cs
+++ Music-Player/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Windows;
+﻿using Music_Player.UserControls;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Music_Player
 {
@@ -9,30 +13,39 @@ namespace Music_Player
     /// </summary>
     public partial class MainWindow : Window
     {
+        ColorPalette p3ColorPalette = new ColorPalette("#2A4AA8", "#0E1272", "#4BA3EB", "#73CCEE", "#CE3633");
+
         public MainWindow()
         {
             InitializeComponent();
+            SetColorPalette(p3ColorPalette);
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if(e.ChangedButton == MouseButton.Left) //If left click is pressed
             {
-
-                this.DragMove(); //drag window
-          }
-
-            /*   resize window
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.Height = 1080;
-                this.Width = 1920;
-            }*/
+                this.DragMove(); //drag window 
+            }
         }
 
         private void CloseButton_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void MaximizeButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void SetColorPalette(ColorPalette colorPalette)
+        {
+            LeftMenu.Background = ColorPainter.SetColorBrush(colorPalette.secondaryBackground);
+            PrimaryBackground.Background = ColorPainter.SetColorBrush(colorPalette.mainBackground);
+            SongsPanel.Background = ColorPainter.SetColorBrush(colorPalette.mainBackground);
+            Player.Background = ColorPainter.SetColorBrush(colorPalette.secondaryBackground);
+            
         }
     }
 }
---
+++ Music-Player/Utilities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Music_Player
+{
+    public static class ColorPainter
+    {
+        public static SolidColorBrush SetColorBrush(string colorHexCode)
+        {
+            SolidColorBrush newColor = new SolidColorBrush();
+            newColor.Color = (Color)ColorConverter.ConvertFromString(colorHexCode);
+            return newColor;
+        }
+    } 
+
+    public struct ColorPalette
+    {
+        public string mainBackground;
+        public string secondaryBackground;
+        public string titleText;
+        public string mainText;
+        public string buttons;
+
+        public ColorPalette(string mainBackground, string leftMenuBackground, string titleText, string mainText, string buttons)
+        {
+            this.mainBackground = mainBackground;
+            this.secondaryBackground = leftMenuBackground;
+            this.titleText = titleText;
+            this.mainText = mainText;
+            this.buttons = buttons;
+        }
+    }
+}
